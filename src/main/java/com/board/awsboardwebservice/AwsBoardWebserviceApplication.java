6fd8908637b964a7d09ede9bd4b4f0b9686201ce
Kim Woonki <woonki94@naver.com>
2022-04-11 10:33:07 +0900

    Initial commit

---
+++ src/main/java/com/board/awsboardwebservice/AwsBoardWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.board.awsboardwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBoardWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBoardWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/awsboardwebservice/AwsBoardWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.awsboardwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBoardWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
